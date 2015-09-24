e6ad2bc311b42cff6b15e282ced3b578b95609cf
Gia Hoang <dev.hoanggia@gmail.com>
2015-09-24 08:15:22 +0700

    giahoang

--- upload/seo_url_change.php
+++ upload/seo_url_change.php
@@ -1,6 +1,9 @@
 <?php
+
 require_once('config.php');
 require_once(DIR_SYSTEM . 'library/db.php');
+require_once(DIR_SYSTEM . 'library/db/'.DB_DRIVER.'.php');
+
 $db = new DB(DB_DRIVER, DB_HOSTNAME, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
 
 // create product alias
